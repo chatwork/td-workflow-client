54ec89bef1d23cc7e420e58849ca67bbccf0206b
cw-kariya <kariya@chatwork.com>
2020-09-17 16:04:23 +0900

    change eslint setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,29 @@
+const prettierRule = require('./prettier.config');
+
 const fileRules = {
-  'prettier/prettier': ['warn', {
-    singleQuote: true,
-    semi: true,
-    printWidth: 100
-  }]
+  'prettier/prettier': ['warn', prettierRule],
+  '@typescript-eslint/no-explicit-any': 'off',
+  '@typescript-eslint/explicit-module-boundary-types': 'off',
 };
 
 module.exports = {
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    'prettier/@typescript-eslint'
-  ],
-  plugins: [
-    '@typescript-eslint',
-    'jest'
-  ],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'plugin:jest/recommended'],
+  plugins: ['@typescript-eslint', 'jest'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2019,
-    sourceType: 'module'
+    ecmaVersion: 2017,
+    sourceType: 'module',
   },
   env: {
-    browser: false,
+    browser: true,
     node: true,
-    es6: false
+    es6: true,
   },
-  rules: fileRules
-}
+  rules: fileRules,
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint'],
+    },
+  ],
+};
